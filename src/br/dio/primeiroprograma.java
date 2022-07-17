5930e2dd3d222b1382efc0e8aff568a04c0a0d1f
majolyne <majo26808@gmail.com>
2022-07-17 16:20:36 -0300

    up

---
+++ src/br/dio/primeiroprograma.java
@@ -0,0 +1,15 @@
+package br.dio;
+
+import br.dio.model.gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+gato gato = new gato ();
+
+        System.out.println(gato);
+        /*int a =5;
+        int b=3;
+        System.out.println(" hello "+ (a+b));*/
+    }
+}
+
